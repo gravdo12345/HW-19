7d7ab25b2e30aaa904ac8b0310e22f71dbd0f618
GRAVDO <gravdo.d@gmail.com>
2024-03-24 16:56:32 +0100

    JESUS IS THE ONLY GOD.

---
+++ src/test/java/MyUnitTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void testAddition() {
+        int result = 1 + 1;
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        int result = 5 - 3;
+        assertEquals(2, result);
+    }
+}
